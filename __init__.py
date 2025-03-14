ac1cd44fb983ee3e9074cb02ad8cf9b908200a2a
runoneall <runoobsteve@hotmail.com>
2025-03-14 09:49:27 +0800

    update

--- __init__.py
+++ __init__.py
@@ -1,3 +1,22 @@
-from . import sdk
+import os
 
-sdk.attr1 = "value1"
+from . import sdk
+from . import util
+
+sdkModulePath = os.path.join(os.path.dirname(__file__), "modules")
+sdkInstalledModules = [
+    __import__("sdkFrame.modules.{}".format(os.path.basename(x)))
+    for x in os.listdir(sdkModulePath)
+    if os.path.isdir(os.path.join(sdkModulePath, x)) and x.startswith("m_")
+]
+
+print(sdkInstalledModules)
+# elements = ["a", "b", "c"]
+# dependencies = {"a": ["b", "c"], "c": ["b"], "b": []}
+
+# try:
+#     sorted_elements = util.topological_sort(elements, dependencies)
+#     print(sorted_elements)
+# except ValueError as e:
+#     print(e)
+#     exit(1)
---
+++ util.py
@@ -0,0 +1,21 @@
+from collections import defaultdict, deque
+
+def topological_sort(elements, dependencies):
+    graph = defaultdict(list)
+    in_degree = {element: 0 for element in elements}
+    for element, deps in dependencies.items():
+        for dep in deps:
+            graph[dep].append(element)
+            in_degree[element] += 1
+    queue = deque([element for element in elements if in_degree[element] == 0])
+    sorted_list = []
+    while queue:
+        node = queue.popleft()
+        sorted_list.append(node)
+        for neighbor in graph[node]:
+            in_degree[neighbor] -= 1
+            if in_degree[neighbor] == 0:
+                queue.append(neighbor)
+    if len(sorted_list) != len(elements):
+        raise ValueError(f"Cycle detected in the dependencies: {elements} -> {dependencies}")
+    return sorted_list
